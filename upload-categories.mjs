09dc46f6c055af9034d87be9dd318ded73826d87
nusretkinici-firmabuldumcom <nusretkinici@gmail.com>
2026-09-14 14:40:37 +0300

    Delete upload-categories.mjs

--- upload-categories.mjs
+++
@@ -1,5 +0,0 @@
-export function uploadType(category){
- const types={z:'Z raporu',income:'Satış faturası',expense:'Fiş'};
- if(!Object.hasOwn(types,category))throw Error('Z raporu, gelir faturası veya gider fişleri kategorisini seç.');
- return types[category];
-}
